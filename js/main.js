1688a9d9ea274ac64460fa8cc2712f259bb0a190
ksunnyoo <sunkbrigita@gmail.com>
2022-12-17 20:03:56 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl =document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus()
-})
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-})
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-})
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -156,7 +139,6 @@ spyEls.forEach(function(spyEl){
     ));
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent =new Date().getFullYear();
+
 
 
